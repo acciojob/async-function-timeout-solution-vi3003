569ff145a7462ebf79dfc57bb6752303c9249ace
Vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-03-25 13:14:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ const btn = document.getElementById("btn");
 const output = document.getElementById("output");
 
 //your code here
+
+  const text = textInput.value;
+      const delayInSeconds = Number(delayInput.value);
+
+      outputDiv.innerText = "Waiting for " + delayInSeconds + " seconds...";
+
+      await new Promise(resolve => setTimeout(resolve, delayInSeconds * 1000));
+
+      outputDiv.innerText = "The message is: " + text;
+    }
+
+    const btn = document.getElementById("btn");
+    btn.addEventListener("click", showMessageAfterDelay);
 async function showMessage() {
   const message = text.value;
   const delayVal = delay.value;
